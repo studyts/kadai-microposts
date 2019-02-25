26220bd6e96942435455cbf8f249d314577255ed
yoshi <studyts744@gmail.com>
2019-02-25 08:44:31 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/', 'MicropostsController@index');
+
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -24,6 +29,5 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 // ユーザ機能
-Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-});
+Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
